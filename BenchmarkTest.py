dcff0f3539c7204e2cb82f951f1e4b0fc2e0770e
Fergus Haak <fergusbhaak@gmail.com>
2021-04-06 00:51:11 +0100

    added cpu info

--- BenchmarkTest.py
+++ BenchmarkTest.py
@@ -1,8 +1,12 @@
 import psutil
+from cpuinfo import get_cpu_info
+import multiprocessing
+import platform
 score = 1
 num = 2
 scores = []
 next = 100
+print("Starting single-core benchmark on: ", get_cpu_info()['brand_raw'])
 cpu = psutil.cpu_percent(interval=0.0000001)
 for x in range(1000):
     while cpu < 50:
@@ -16,14 +20,19 @@ for x in range(1000):
         
     cpu = 0
     score = 0
-
-
 finalScore = sum(scores) / len(scores)
+print("done")
+print()
+print(platform.processor())
+("----------------------------------")
 print("CPU final score average: ", finalScore)
-request = input("type 1 to see all scores")
-if request == "1":
+print("----------------------------------")
+print("type 1 to view technical cpu info")
+request = input("type 2 to see all scores: ")
+if request == "2":
     print(scores)
-    
+if request == "1":
+    print(get_cpu_info())
 
 
 
