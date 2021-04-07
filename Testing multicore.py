82496837f16ef4b9416039da8dd735d00ae10ed2
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-04-07 20:08:18 +0100

    Update Testing multicore.py

--- Testing multicore.py
+++ Testing multicore.py
@@ -1,5 +1,7 @@
 if __name__ == '__main__': #used to allow or prevent parts of code from being run when the modules are imported.
+    import multiprocessing #allows use of multiple cores
     import time
+    
 
     start = time.perf_counter()
 
@@ -8,12 +10,15 @@ if __name__ == '__main__': #used to allow or prevent parts of code from being ru
         time.sleep(1)
         print('done sleeping...')
 
-    do_something()#runs once takes 1 sec, if done twice it will take 2 sec.
-    do_something()#runs after the one before
+    p1 = multiprocessing.Process(target=do_something) #creates first procces
+    p2 = multiprocessing.Process(target=do_something) #creates second process at same time
+
+    p1.start()
+    p2.start()#starts both processes at same time does not wait to continue onto rest of code
 
     finnish = time.perf_counter()
 
     print(f'Finnished in {round(finnish-start, 2)} second(s)') 
-    #finnishes in 1 sec since it only does do-something once, if it was run twice it would take 2 secs.
+    
 
 #here we run both tasks at once so that it only takes 1 sec instead of 2 since do_somthing is running in parrallel
