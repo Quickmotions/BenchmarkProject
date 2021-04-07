d08560114dd71fb07d5dec1f118cec8b8f477648
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-04-07 19:13:49 +0100

    working on multi core

--- CPUBenchmark - Multicore.py
+++ CPUBenchmark - Multicore.py
@@ -13,24 +13,26 @@ score = 1
 num = 2
 scores = []
 next = 100
-print("Starting single-core benchmark on: ", get_cpu_info()['brand_raw'])
-cpu = psutil.cpu_percent(interval=0.0000001)
-for x in range(1000):
-    while cpu < 50:
-        cpu = psutil.cpu_percent(interval=0.0)
-        num = 2**score
-        score += 1
-    scores.append(score)
-    if x == next:
-        print(next / 10, "% done")
-        next += 100 
-    cpu = 0
-    score = 0
+
+def SingleBench():
+    cpu = psutil.cpu_percent(interval=0.0000001)
+    for x in range(1000):
+        while cpu < 50:
+            cpu = psutil.cpu_percent(interval=0.0)
+            num = 2**score
+            score += 1
+        scores.append(score)
+        if x == next:
+            print(next / 10, "% done")
+            next += 100 
+        cpu = 0
+        score = 0
 
 
 
 
 
+print("Starting multi-core benchmark on: ", get_cpu_info()['brand_raw'])
 
 cpuScore = sum(scores) / len(scores)
 print("done")
