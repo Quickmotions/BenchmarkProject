82051ca5c91932a07a4e69266d2007e5cf051f8e
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-05-23 19:23:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from multiCPU import runMultiCPU
-from singleCPU import runSingleCPU
-from writeCSV import runWriteCSV
-
-
-
-runSingleCPU()
-runMultiCPU()
-runWriteCSV()
+def main():
+    results = ["linux", "intel i8", 123, 234]
+    
+    from multiCPU import runMultiCPU
+    from singleCPU import runSingleCPU
+    from writeCSV import runWriteCSV
+    runSingleCPU()
+    runMultiCPU()
+    runWriteCSV(results)
 
 
