9a9d84c13a079df267671e417c0cf694f65b37e5
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-05-23 20:04:05 +0100

    fix

--- singleCPU.py
+++ singleCPU.py
@@ -1,3 +1,11 @@
-import psutil
 def runSingleCPU():
-    return "works"
+    import psutil
+    import time
+    import math
+    starttime = time.time() # start timer
+    count = 0
+    while count < 100000:
+        piValue = 2 ** count
+        count += 1
+    timeTaken = str(round((time.time() - starttime), 2)) + ' Seconds'
+    print(timeTaken)
