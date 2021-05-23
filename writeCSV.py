40355c3f4babf51231b5b99f76beb3a1ff26217d
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-05-23 19:25:33 +0100

    Update writeCSV.py

--- writeCSV.py
+++ writeCSV.py
@@ -1,9 +1,8 @@
 # runs the import to bring in the csv settings required
 import csv
 
-def runWriteCSV():
+def runWriteCSV(results):
     if __name__ == '__main__':
-# printing to file 
-        with open('.../csv_files/data_container.csv', 'a', newline ='') as x:
-            file_writer = csv.writer(x)
-            file_writer.writerow([results[0],results[1],results[2],results[3],results[4],results[5],results[6],results[7],results[8]])
+        with open('.../csv_files/data_container.csv', 'a', newline ='') as file:
+            file_writer = csv.writer(file) 
+            file_writer.writerow(results)
