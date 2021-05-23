3d2044780a9e3c4f68f9a7ed327e46ea0d936934
Callum <65352339+callumgourlay@users.noreply.github.com>
2021-05-23 19:20:42 +0100

    Update writeCSV.py

--- writeCSV.py
+++ writeCSV.py
@@ -1,21 +1,8 @@
 # runs the import to bring in the csv settings required
 import csv
 
-# Declaring String Variables
-benchmarkID = "Placeholder"
-system = "Placeholder"
-cpuDetails = "Placeholder"
-# Declaring Float Variables
-cpuScore = 0.00
-gpuScore = 0.00
-storageScore = 0.00
-ramScore = 0.00
-overallScore = 0.00
-multiCPUScore = 0.00
-singleCPUScore = 0.00
-
 def runWriteCSV():
 # printing to file 
     with open('.../csv_files/data_container.csv', 'a', newline ='') as x:
         file_writer = csv.writer(x)
-        file_writer.writerow([benchmarkID,system,singleCPUScore,multiCPUScore,gpuScore,storageScore,ramScore,overallScore,cpuDetails])
+        file_writer.writerow([results[0],results[1],results[2],results[3],results[4],results[5],results[6],results[7],results[8]])
