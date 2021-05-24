1471885a2c30599e145f1d05860cf562879425f7
Fergus Haak <67606548+Quickmotions@users.noreply.github.com>
2021-05-24 13:05:07 +0100

    Update singleCPU.py

--- singleCPU.py
+++ singleCPU.py
@@ -4,8 +4,9 @@ def runSingleCPU():
     import math
     starttime = time.time() # start timer
     count = 0
-    while count < 100000:
+    while count < 100000: # run x amount of calculations
         piValue = 2 ** count
         count += 1
-    timeTaken = str(round((time.time() - starttime), 2)) + ' Seconds'
-    print(timeTaken)
+    timeTaken = round((time.time() - starttime, 3) #find time taken for x calculations to perform.
+    print(str(timeTaken) + " seconds") #print seconds taken#
+    return timeTaken
