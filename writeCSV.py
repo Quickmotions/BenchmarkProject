cde2f2cbd4f7ecf43d31fcf21da9d903446cbc1d
Fergus Haak <67606548+Quickmotions@users.noreply.github.com>
2021-05-25 09:34:06 +0100

    Update writeCSV.py

--- writeCSV.py
+++ writeCSV.py
@@ -1,8 +1,50 @@
-def runWriteCSV(results):
-    
-    import csv          #import csv read/writer
-    import cryptography #import encryption package
+def createKey():
+    from cryptography.fernet import Fernet  #import encryption package
 
+    key = Fernet.generate_key() #create encryption key
+    
+    with open('filekey.key', 'wb') as filekey:  #open a key file
+        filekey.write(key)                      #write the key into the file (ex. key: J64ZHFpCWFlS9zT7y5zxuQN1Gb09y7cucne_EhuWyDM=)    
+
+        
+        
+        
+        
+def encryptResults():       #encrypt the file using fernet
+    
+    from cryptography.fernet import Fernet  #import encryption package
+    
+    # opening the key
+    with open('filekey.key', 'rb') as filekey:
+        key = filekey.read()
+
+    # using the generated key
+    fernet = Fernet(key)
+
+    # opening the original file to encrypt
+    with open('nba.csv', 'rb') as file:
+        original = file.read()
+
+    # encrypting the file
+    encrypted = fernet.encrypt(results)
+
+    # opening the file in write mode and 
+    # writing the encrypted data
+    with open('csv_files/data_container_encrypted.csv', 'wb') as encrypted_file:
+        encrypted_file.write(encrypted)
+
+        
+        
+        
+        
+def runWriteCSV(results):
+    import csv   #import csv read/writer
+    createKey()  #create a key and store it
+    
     with open('csv_files/data_container.csv', 'a', newline ='') as file: #open/create new file
         file_writer = csv.writer(file)  #select csv writer
         file_writer.writerow(results)   #write the results into the csv
+       
+    encryptResults()
+    
+    print("Done")
