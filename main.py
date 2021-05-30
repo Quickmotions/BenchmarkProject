fd2d314b9865ff42339d2ab2dd045be108374553
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-05-30 09:34:19 +0100

    moved multicpu

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
         from singleCPU import runSingleCPU
         from writeCSV import runWriteCSV
         from memoryTest import runMemoryTest
+        from multiCPU import spawnMultiProcess
         from multiprocessing import Process
         import time
     except:
@@ -11,29 +12,29 @@ if __name__ == '__main__':
         exit()
 
     
-def multiProcess():
-    count = 0
-    while count < 5000: # run x amount of calculations (x can be adjusted to get diffrent scores)
-         piValue = 2 ** count        #(2*increasing value) increasing value increases by 1 each loop and loops for x amount
-         count += 1
+# def multiProcess():
+#     count = 0
+#     while count < 5000: # run x amount of calculations (x can be adjusted to get diffrent scores)
+#          piValue = 2 ** count        #(2*increasing value) increasing value increases by 1 each loop and loops for x amount
+#          count += 1
 
-def spawnMultiProcess():
-    if __name__ == '__main__':  # confirms that the code is under main function
-        print("DEBUG: begun multiCPU")
-        start = time.perf_counter()
-        procs = []
-        # instantiating process with arguments
-        for _ in range(100): 
-            proc = Process(target=multiProcess)
-            procs.append(proc)
-            proc.start()
+# def spawnMultiProcess():
+#     if __name__ == '__main__':  # confirms that the code is under main function
+#         print("DEBUG: begun multiCPU")
+#         start = time.perf_counter()
+#         procs = []
+#         # instantiating process with arguments
+#         for _ in range(100): 
+#             proc = Process(target=multiProcess)
+#             procs.append(proc)
+#             proc.start()
 
-        # complete the processes
-        for proc in procs:
-            proc.join()
+#         # complete the processes
+#         for proc in procs:
+#             proc.join()
 
-        timeTaken = round(time.perf_counter() - start, 2)
-        return round(10000 /timeTaken/10)
+#         timeTaken = round(time.perf_counter() - start, 2)
+#         return round(10000 /timeTaken/10)
 
 def main():
     results = []                        #create array for all results 
--- multiCPU.py
+++ multiCPU.py
@@ -1,5 +1,28 @@
+def multiProcess():
+    count = 0
+    while count < 5000: # run x amount of calculations (x can be adjusted to get diffrent scores)
+         piValue = 2 ** count        #(2*increasing value) increasing value increases by 1 each loop and loops for x amount
+         count += 1
 
+def spawnMultiProcess():
+    if __name__ == 'multiCPU':  # confirms that the code is under main function
+        from multiprocessing import Process
+        import time
+        print("DEBUG: begun multiCPU")
+        start = time.perf_counter()
+        procs = []
+        # instantiating process with arguments
+        for _ in range(100): 
+            proc = Process(target=multiProcess)
+            procs.append(proc)
+            proc.start()
 
+        # complete the processes
+        for proc in procs:
+            proc.join()
+
+        timeTaken = round(time.perf_counter() - start, 2)
+        return round(10000 /timeTaken/10)
 
 
    
