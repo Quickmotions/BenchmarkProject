045fb427c5eacf22219f20153c439ef88ac61f15
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-05-30 11:20:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import Label, font
 if __name__ == '__main__':
     try:
         from getComp import runGetComp
@@ -5,15 +6,45 @@ if __name__ == '__main__':
         from writeCSV import runWriteCSV
         from memoryTest import runMemoryTest
         from multiCPU import spawnMultiProcess
-        from multiprocessing import Process
-        import time
+        import tkinter as tk
     except:
         print("missing packages")
         exit()
 
+class UI():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.frame = tk.Frame(self.root)
+        self.root.geometry("600x500")
+        self.frame.pack()
+        self.text = Label(self.root, text="Press start to begin", height=10)
+        self.text.pack()
+        self.button = tk.Button(self.frame, 
+                        text="QUIT", 
+                        fg="red",
+                        command=quit,
+                        width=30,
+                        height=20)
+        self.button.pack(side=tk.LEFT,)
+        self.slogan = tk.Button(self.frame,
+                        text="START",
+                        command= main,
+                        width=30,
+                        height=20)
+        self.slogan.pack(side=tk.LEFT)
+
+        self.root.mainloop()
+
+    def changeText(self):
+        self.text['text'] = "textShown"       
+
+
+
+#change ui text
 def main():
     #create array for all results
     results = []                        
+    UI().changeText()
 
     #get all system info and store in a list, concatonate list into results
     systemTag, system, machine, version, release, node,cpuTag, cpuBrand, physCore, allCore,memoryTag, memTotal, memUsed,storageTag, storageDevice = runGetComp()    #collect all system and component imfo
@@ -27,7 +58,6 @@ def main():
          
     #store all the reults in the csv     
     runWriteCSV(results)               
-
-
-if __name__ == '__main__':
-    main()  #call main to start program (if it is main and not a multiproccess)
+#TKINTER UI BELOW
+if __name__ == '__main__': 
+   app=UI()
