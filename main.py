a6ae37b8992edf262ebc5d2883e171108ec59a79
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-05-30 09:43:52 +0100

    sort code

--- main.py
+++ main.py
@@ -12,22 +12,22 @@ if __name__ == '__main__':
         exit()
 
 def main():
-    results = []                        #create array for all results 
+    #create array for all results
+    results = []                        
 
+    #get all system info and store in a list, concatonate list into results
     systemTag, system, machine, version, release, node,cpuTag, cpuBrand, physCore, allCore,memoryTag, memTotal, memUsed,storageTag, storageDevice = runGetComp()    #collect all system and component imfo
     compList =[systemTag, system, machine, "ver: "+str(version), release, node,cpuTag, "cpu: "+str(cpuBrand), "physical: "+str(physCore), "all: "+str(allCore),memoryTag, "total: "+str(memTotal), "used: "+str(memUsed),storageTag, storageDevice]            #store all spec
     results += compList
 
-    singleResult = runSingleCPU()       #run a single core cpu benchmark
-    results.append("singleCPU: "+str(singleResult))        #store the result
-    
-    multiResult = spawnMultiProcess()          #run a multi core cpu benchmark
-    results.append("multiCPU: "+str(multiResult))           #store the result
-    
-    memoryResult = runMemoryTest()      #run the memory usage test
-    results.append("memory: "+str(memoryResult))        #store the result
-    
-    runWriteCSV(results)                #store all the reults in the csv
+    #add values of each test
+    results.append("singleCPU: "+str(runSingleCPU()))      
+    results.append("multiCPU: "+str(spawnMultiProcess()))  
+    results.append("memory: "+str(runMemoryTest()))   
+         
+    #store all the reults in the csv     
+    runWriteCSV(results)               
+
 
 if __name__ == '__main__':
-    main()  #call main to start program
+    main()  #call main to start program (if it is main and not a multiproccess)
