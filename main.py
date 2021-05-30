07d9d2321053eeeec4b90c66ba58ad5516686edd
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-05-30 12:07:21 +0100

    not done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import Label, font
+import tkinter
+from typing import NewType
 if __name__ == '__main__':
     try:
         from getComp import runGetComp
@@ -6,46 +8,20 @@ if __name__ == '__main__':
         from writeCSV import runWriteCSV
         from memoryTest import runMemoryTest
         from multiCPU import spawnMultiProcess
-        import tkinter as tk
+        from tkinter import *
     except:
         print("missing packages")
         exit()
 
-class UI():
-    def __init__(self):
-        self.root = tk.Tk()
-        self.frame = tk.Frame(self.root)
-        self.root.geometry("600x500")
-        self.frame.pack()
-        self.text = Label(self.root, text="Press start to begin", height=10)
-        self.text.pack()
-        self.button = tk.Button(self.frame, 
-                        text="QUIT", 
-                        fg="red",
-                        command=quit,
-                        width=30,
-                        height=20)
-        self.button.pack(side=tk.LEFT,)
-        self.slogan = tk.Button(self.frame,
-                        text="START",
-                        command= main,
-                        width=30,
-                        height=20)
-        self.slogan.pack(side=tk.LEFT)
 
-        self.root.mainloop()
+def changeText(new):
+    newText.set(new)
 
-    def changeText(self):
-        self.text['text'] = "textShown"       
-
-
-
-#change ui text
 def main():
     #create array for all results
-    results = []                        
-    UI().changeText()
-
+    results = []                      
+    changeText("starting getComp")
+    
     #get all system info and store in a list, concatonate list into results
     systemTag, system, machine, version, release, node,cpuTag, cpuBrand, physCore, allCore,memoryTag, memTotal, memUsed,storageTag, storageDevice = runGetComp()    #collect all system and component imfo
     compList =[systemTag, system, machine, "ver: "+str(version), release, node,cpuTag, "cpu: "+str(cpuBrand), "physical: "+str(physCore), "threads: "+str(allCore),memoryTag, "total: "+str(memTotal), "used: "+str(memUsed),storageTag, storageDevice]            #store all spec
@@ -59,5 +35,27 @@ def main():
     #store all the reults in the csv     
     runWriteCSV(results)               
 #TKINTER UI BELOW
-if __name__ == '__main__': 
-   app=UI()
+
+root = tkinter.Tk()
+frame = tkinter.Frame(root)
+root.geometry("600x500")
+frame.pack()
+newText = StringVar()
+newText.set("press start")
+text = Label(root, textvariable=newText)
+text.pack()
+button = tkinter.Button(frame, 
+                text="QUIT", 
+                fg="red",
+                command=quit,
+                width=30,
+                height=20)
+button.pack(side=tkinter.LEFT,)
+slogan = tkinter.Button(frame,
+                text="START",
+                command= main,
+                width=30,
+                height=20)
+slogan.pack(side=tkinter.LEFT)
+
+root.mainloop()
