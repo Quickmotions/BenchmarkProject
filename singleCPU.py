f3092fa60ddf4f371e6b62d69073d5be8be36644
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-05-31 11:01:16 +0100

    added debug testing options

--- singleCPU.py
+++ singleCPU.py
@@ -9,7 +9,7 @@ def runSingleCPU():
         piValue = 2 ** count        #(2*increasing value) increasing value increases by 1 each loop and loops for x amount
         count += 1
     timeTaken = round(float(time.time() - starttime), 3) #find time taken for x calculations to perform.
-    
+    print("DEBUG singletime: " + str(timeTaken))
     #if time is < 1 then make the score max(10000) this is because if the time is less than 1 then the max score will be multiplied instead of reduced
     if timeTaken > 1:               
         score = 10000 / timeTaken   #to get score divide 10000 by the time it took to run x calculations
