14d5cc057497d8fc5f51b6442a5e838036d49349
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-06-10 13:44:57 +0100

    Update singleCPU.py

--- singleCPU.py
+++ singleCPU.py
@@ -10,26 +10,21 @@ def runSingleCPU():
     count = 0
     score1 = 1
     cpu = psutil.cpu_percent(interval=0.01)
-    print(cpu)
    
     while count < 50000: # run x amount of calculations (x can be adjusted to get diffrent scores)
         piValue = 2 ** count        #(2*increasing value) increasing value increases by 1 each loop and loops for x amount
         count += 1
     timeTaken = round(float(time.time() - starttime), 3) #find time taken for x calculations to perform.
-    print("DEBUG singletime: " + str(timeTaken))
     #if time is < 1 then make the score max(10000) this is because if the time is less than 1 then the max score will be multiplied instead of reduced
     if timeTaken > 1:               
         score = 10000 / timeTaken   #to get score divide 10000 by the time it took to run x calculations
     else:
         score = 10000
 
-    while cpu < 200:
-            cpu = psutil.cpu_percent(interval=0.00001)
-            num = score1**score1
+    while cpu < 100:
+            cpu = psutil.cpu_percent(interval=0.01)
+            num = 2**score1
             score1 += 1
+    return round(score/10 + score1/10 / 2) #return the score and round to nearest whole
 
-
-    avg = round(score/10 + score1/10 / 2)
-    print(avg)
-    print(cpu)
-    return avg #return the score and round to nearest whole
+runSingleCPU()
