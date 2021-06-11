065650e0349cd92ce43d5b96e8559a14556f9163
Callum Gourlay <callum@callumg.com>
2021-06-11 10:21:57 +0100

    database testing

--- Experimental/dbtesting.py
+++ Experimental/dbtesting.py
@@ -1,2 +1,15 @@
+import csv
 import psycopg2 
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
+conn = psycopg2.connect("host=ec2-54-216-17-9.eu-west-1.compute.amazonaws.com dbname=d6jfdmfbk8m22t user=ydngonadspindw password=132f2ff02391469a239e18f0ef884109a2dc7b5cea958ae82ae7851b6229f29d")
+cur = conn.cursor()
+insert_query = "INSERT INTO test VALUES {}".format("(10, 'hello@callumg.com', 'Some NamE', '123 Fake St.')")
+select_query = "SELECT * FROM test"
+with open('Experimental/user.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader) # Skip the header row.
+    for row in reader:
+        cur.execute(
+        "INSERT INTO test VALUES (%s, %s, %s, %s)",
+        row
+    )
+conn.commit()
